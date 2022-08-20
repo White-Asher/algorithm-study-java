52d7b8bb8d5000720a3712965a19c2540d2a91d7
김성태 <joykst96@gmail.com>
2022-08-21 00:41:13 +0900

    Solve BOJ_3190

--- 4주차/BOJ_3190_뱀/BOJ_3190_김성태.java
+++ 4주차/BOJ_3190_뱀/BOJ_3190_김성태.java
@@ -1,91 +1,92 @@
 package com.joykst96.practice.boj;
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Pos {
-	int x;
-	int y;
-	
-	Pos(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (!(obj instanceof Pos)) return false;
-		return this.x  == ((Pos)obj).x && this.y  == ((Pos)obj).y;
-	}
-	
-	@Override
-	public int hashCode() {
-		return toString().hashCode();
-	}
-	
-	@Override
-	public String toString() {
-		return String.format("[%d, %d]", x, y);
-	}
-}
+/*
+ *  메모리: 25584 KB
+ *  시간: 336 ms
+ * 
+ */
 
-public class BOJ_3190 {
+public class BOJ_3190_뱀 {
+	static class Location {
+		int r, c;
+		
+		Location(int r, int c){
+			this.r = r;
+			this.c = c;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == null || !(obj instanceof Location)) return false;
+			if (this.r == ((Location)obj).r && this.c == ((Location)obj).c) return true;
+			return false;
+		}
+		
+		@Override
+		public int hashCode() {
+			return toString().hashCode();
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("[%d, %d]", r, c);
+		}
+	}
 	
-	static Set<Pos> apples;
-	static Deque<Pos> snake;
-	static int head, time, N, size;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
+	static int N, head, time;
+	static int[] dr = {0, 1, 0, -1};
+	static int[] dc = {1, 0, -1, 0};
+	static Set<Location> apples;
+	static Deque<Location> snake;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		N = Integer.parseInt(br.readLine()) + 1;
+		N = Integer.parseInt(br.readLine());
+		int K = Integer.parseInt(br.readLine());
 		apples = new HashSet<>();
-		for (int i = 0, K = Integer.parseInt(br.readLine()); i < K; i++) {
+		for (int i = 0; i < K; i++) {
 			st = new StringTokenizer(br.readLine());
-			apples.add(new Pos(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1));
+			apples.add(new Location(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1));
 		}
-		Map<Integer, Character> tasks = new HashMap<>();
-		for (int i = 0, n = Integer.parseInt(br.readLine()); i < n; i++) {
+		Map<Integer, Integer> events = new HashMap<>();
+		int L = Integer.parseInt(br.readLine());
+		for (int i = 0; i < L; i++) {
 			st = new StringTokenizer(br.readLine());
-			tasks.put(Integer.parseInt(st.nextToken()), st.nextToken().charAt(0));
+			events.put(Integer.parseInt(st.nextToken()), st.nextToken().charAt(0) == 'D' ? 1 : -1);
 		}
 		snake = new LinkedList<>();
-		snake.offer(new Pos(0, 0));
+		snake.add(new Location(0, 0));
 		head = 0;
 		time = 0;
 		while (true) {
-			if (tasks.get(time) == null) {
-				move();
-			}
-			else {
-				turn(tasks.get(time));
-				move();
-			}
+			++time;
+			move();
+			if (events.get(time) != null) turn(events.get(time));
 		}
+		
 	}
-
+	
 	private static void move() {
-		++time;
-		Pos headPos = snake.peek();
-		Pos nP = new Pos(headPos.x + dx[head], headPos.y + dy[head]);
-		if (!isIn(nP) || snake.contains(nP)) {
+		Location cur = snake.peekFirst();
+		Location next = new Location(cur.r + dr[head], cur.c + dc[head]);
+		if (!isIn(next) || snake.contains(next)) {
 			System.out.println(time);
 			System.exit(0);
 		}
-		snake.offerFirst(nP);
-		if (apples.contains(nP)) apples.remove(nP);
+		snake.offerFirst(next);
+		if (apples.contains(next)) apples.remove(next);
 		else snake.pollLast();
 	}
 
-	private static void turn(char task) {
-		if (task == 'L') --head;
-		else ++head;
-		head %= 4;
+	private static void turn(int d) {
+		head = (head + 4 + d) % 4;
 	}
 	
-	private static boolean isIn(Pos p) {
-		return (p.x >= 0 && p.y >= 0 && p.x < N && p.y < N); 
+	private static boolean isIn(Location l) {
+		return (l.r >= 0 && l.c >= 0 && l.r < N && l.c < N);
 	}
 }
