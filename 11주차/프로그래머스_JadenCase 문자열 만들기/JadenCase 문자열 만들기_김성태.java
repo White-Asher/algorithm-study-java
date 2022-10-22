6287cdef805ba400576e6b988922fc61238d15a7
김성태 <joykst96@gmail.com>
2022-10-22 21:10:46 +0900

    Solve week 11 assignment

--- 11주차/프로그래머스_JadenCase 문자열 만들기/JadenCase 문자열 만들기_김성태.java
+++ 11주차/프로그래머스_JadenCase 문자열 만들기/JadenCase 문자열 만들기_김성태.java
@@ -1,27 +1,32 @@
 package com.joykst96.practice.programmers;
 
-import java.util.StringTokenizer;
+import com.joykst96.practice.programmers.PGS_12951_JadenCase_문자열_만들기.Solution;
 
-public class PGS_12939_최댓값과_최솟값 {
-	
+public class PGS_12951_JadenCase_문자열_만들기 {
 	static class Solution {
 	    public String solution(String s) {
-	        StringTokenizer st = new StringTokenizer(s, " ");
-	        int min = Integer.MAX_VALUE;
-	        int max = Integer.MIN_VALUE;
-	        while (st.hasMoreTokens()) {
-	            int next = Integer.parseInt(st.nextToken());
-	            min = Math.min(next, min);
-	            max = Math.max(next, max);
-	        }
-	        return String.format("%d %d", min, max);
+	    	char[] cArr = s.toCharArray();
+	    	boolean flag = true;
+	    	for (int i = 0; i < cArr.length; ++i) {
+	    		if (cArr[i] == ' ') {
+	    			flag = true;
+	    		} else if (flag) {
+	    			cArr[i] = 'a' <= cArr[i] && cArr[i] <= 'z' ? (char)(cArr[i] - ('a' - 'A')) : cArr[i];
+	    			flag = false;
+	    		} else {
+	    			cArr[i] = 'A' <= cArr[i] && cArr[i] <= 'Z' ? (char)(cArr[i] + ('a' - 'A')) : cArr[i];
+	    		}
+	    	}
+	    	return String.valueOf(cArr);
 	    }
 	}
 	
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		System.out.println(solution.solution("1 2 3 4"));
-		System.out.println(solution.solution("-1 -2 -3 -4"));
-		System.out.println(solution.solution("-1 -1"));
+		System.out.println(solution.solution("3people unFollowed me"));
+		System.out.println(solution.solution("for the last week"));
+		System.out.println(solution.solution(" "));
+		System.out.println(solution.solution(" aa"));
+		System.out.println(solution.solution(" a     a bb c   d"));
 	}
 }
