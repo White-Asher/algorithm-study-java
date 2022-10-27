3753bac49dc20edf58b3318ae21ab3ef823d31aa
김명준 <kmj0568@gmail.com>
2022-10-27 21:51:48 +0900

    Solve: Programers - 최댓값과 최솟값, JadenCase 문자열 만들기

---
+++ 11주차/프로그래머스_JadenCase 문자열 만들기/JadenCase 문자열 만들기_김명준.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String str = " a         bc ";
+        System.out.println(solution(str));
+
+    }
+
+    static public String solution(String s) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        s = s.toLowerCase();
+
+        boolean flag = true;
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            // 공백이 나오면 다음 첫 문자를 보고 바꿀 준비
+            if (cur == ' ') {
+                flag = true;
+            } else {
+                if (flag && cur >= 97 && cur <= 122) {
+                    cur -= 32;
+                }
+                flag = false;
+            }
+            sb.append(cur);
+        }
+
+        answer = sb.toString();
+
+        return answer;
+    }
+}
